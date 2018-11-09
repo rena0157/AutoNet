9dfae9d7952304e1343f517c339d60c98ce79c0a
Adam Renaud <adamf.renaud@gmail.com>
2018-11-09 10:49:30 -0500

    Fixing Namespace

--- AutoNet/AutoNet.cs
+++ AutoNet/AutoNet.cs
@@ -16,7 +16,7 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
 
-namespace AutoListAC
+namespace AutoNet
 {
     public static class AutoNet
     {
